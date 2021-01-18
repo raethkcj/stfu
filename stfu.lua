d3b5903e0d1f8ff621fb90a1473b64e37442294c
Casey Raethke <casey.j.raethke@gmail.com>
2021-01-18 10:17:30 -0600

    Add Details, and make better locale and nil checks

--- stfu.lua
+++ stfu.lua
@@ -1,3 +1,15 @@
-local NovaWorldBuffsLocale = LibStub("AceLocale-3.0"):NewLocale("NovaWorldBuffs", "enUS", true, true)
+local NovaWorldBuffsLocale = LibStub("AceLocale-3.0"):GetLocale("NovaWorldBuffs", true)
+if NovaWorldBuffsLocale then
+	NovaWorldBuffsLocale["versionOutOfDate"] = ""
+end
 
-NovaWorldBuffsLocale["versionOutOfDate"] = ""
+local _detalhes = _G.detalhes
+if _detalhes then
+	local DetailsLocale = LibStub("AceLocale-3.0"):GetLocale("Details")
+	local _detalhes_Msg_Original = _detalhes.Msg
+	function _detalhes:Msg(_string, ...)
+		if _string ~= DetailsLocale["STRING_VERSION_AVAILABLE"] then
+			_detalhes_Msg_Original(_string, ...)
+		end
+	end
+end
