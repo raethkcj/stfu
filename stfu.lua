ec3a5c7867fdccc8f110b95c107eb34c0c4442dc
Casey Raethke <casey.j.raethke@gmail.com>
2021-03-24 13:09:15 -0500

    Hook AddMessage instead of trying to manipulate each addon separately.

--- stfu.lua
+++ stfu.lua
@@ -1,15 +1,26 @@
-local NovaWorldBuffsLocale = LibStub("AceLocale-3.0"):GetLocale("NovaWorldBuffs", true)
-if NovaWorldBuffsLocale then
-	NovaWorldBuffsLocale["versionOutOfDate"] = ""
+local blocked_messages = {}
+
+local AddMessage = DEFAULT_CHAT_FRAME.AddMessage
+DEFAULT_CHAT_FRAME.AddMessage = function(frame, message, ...)
+	if not blocked_messages[message] then
+		AddMessage(frame, message, ...)
+	end
 end
 
-local _detalhes = _G.detalhes
-if _detalhes then
-	local DetailsLocale = LibStub("AceLocale-3.0"):GetLocale("Details")
-	local _detalhes_Msg_Original = _detalhes.Msg
-	function _detalhes:Msg(_string, ...)
-		if _string ~= DetailsLocale["STRING_VERSION_AVAILABLE"] then
-			_detalhes_Msg_Original(_string, ...)
-		end
+if IsAddOnLoaded("NovaWorldBuffs") then
+	local L = LibStub("AceLocale-3.0"):GetLocale("NovaWorldBuffs", true)
+	if L then
+		blocked_messages["|cFFFF6900" .. L["versionOutOfDate"]] = true
 	end
+end
+
+if IsAddOnLoaded("Details") then
+	local L = LibStub("AceLocale-3.0"):GetLocale("Details")
+	if L then
+		blocked_messages[L["STRING_VERSION_AVAILABLE"]] = true
+	end
+end
+
+if IsAddOnLoaded("aux-addon") then
+	blocked_messages[LIGHTYELLOW_FONT_COLOR_CODE .. '<aux> ' .. 'loaded - /aux'] = true
 end
